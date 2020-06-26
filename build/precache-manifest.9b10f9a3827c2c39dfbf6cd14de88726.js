46d0de8544f91be74edcee37312ddefc5fa6f27a
Chaitanyakrishna <chaitanyadukkipaty@gmail.com>
2020-06-26 19:18:02 +0530

    changes

---
+++ build/precache-manifest.9b10f9a3827c2c39dfbf6cd14de88726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0a4518020798fede07f68a5196a645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99466a0742d0cfe6c5a9",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "34c2b9c9566c8974414d",
+    "url": "/static/css/main.519e9230.chunk.css"
+  },
+  {
+    "revision": "99466a0742d0cfe6c5a9",
+    "url": "/static/js/2.99483ed4.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.99483ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c2b9c9566c8974414d",
+    "url": "/static/js/main.19db665c.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
